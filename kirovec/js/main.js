c55dc0ee5407d8a7c487b129f8827555d74c90b1
Vladimir <159266280+Amedai@users.noreply.github.com>
2024-02-20 17:02:10 +0300

    add interactive with modal and links

--- kirovec/js/main.js
+++ kirovec/js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+    //burger menu
     const inner = document.querySelector('.header__inner'),
     hamburger = document.querySelector('.header__mobile-burger'),
     sticks = document.querySelectorAll('.header__mobile-stick');
@@ -9,4 +10,53 @@ window.addEventListener('DOMContentLoaded', () => {
             item.classList.toggle('header__mobile-stick_active');
         });
     });
+
+    //modal
+    function fadeIn (el, timeout, display){
+        el.style.opacity = 0;
+        el.style.display = display || 'block';
+        el.style.transition = `opacity ${timeout}ms`;
+        setTimeout(() => {
+          el.style.opacity = 1;
+        }, 10);
+    }
+    function fadeOut (el, timeout){
+        el.style.opacity = 1;
+        el.style.transition = `opacity ${timeout}ms`;
+        el.style.opacity = 0;
+      
+        setTimeout(() => {
+          el.style.display = 'none';
+        }, timeout);
+    }
+    const contactButtons = document.querySelectorAll('.contact-btn'),
+            priceButtons = document.querySelectorAll('.price-btn'),
+            overlay = document.querySelector('.overlay'),
+            closeElement = document.querySelector('.modal__close');
+    
+    contactButtons.forEach(btn=>{
+        btn.addEventListener('click',()=>{
+            fadeIn(overlay, 100, 'block');
+        });
+    });
+    priceButtons.forEach(btn=>{
+        btn.addEventListener('click',()=>{
+            fadeIn(overlay, 100, 'block');
+        });
+    });
+    closeElement.addEventListener('click',()=>{
+        fadeOut(overlay,30);
+    });
+    overlay.addEventListener('click', (e)=>{
+        const target = e.target;
+        if(target && target.classList.contains('overlay')){
+            fadeOut(overlay,30);
+        }
+    });
+    document.addEventListener('keydown',(e)=>{
+        if(overlay.style.opacity === '1' && e.code == 'Escape'){
+           fadeOut(overlay,30);
+        }
+     });
+
 });
