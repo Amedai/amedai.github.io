4499e69d4b1d151dc54d413dd3b07e7cde3a57bb
Vladimir <159266280+Amedai@users.noreply.github.com>
2024-02-23 21:59:04 +0300

    Add files via upload

--- kirovec/js/main.js
+++ kirovec/js/main.js
@@ -59,4 +59,108 @@ window.addEventListener('DOMContentLoaded', () => {
         }
      });
 
+
+    //слайдер
+    const promotionInfoSlider = new Swiper('.promotion__info-slider', {
+        loop: true,
+        slidesPerView: 'auto',
+        grabCursor: true,
+        pagination: {
+          el: '.info-slider__pagination',
+          bulletClass: 'info-slider__bullet',
+          bulletActiveClass: 'info-slider__bullet-active'
+        },
+      
+        navigation: {
+          nextEl: '.info-slider__right',
+          prevEl: '.info-slider__left',
+        },
+    });
+
+    const tractorInfoSlider = new Swiper('.tractor__info-slider', {
+        loop: true,
+        slidesPerView: 'auto',
+        grabCursor: true,
+        pagination: {
+          el: '.info-slider__pagination',
+          bulletClass: 'info-slider__bullet',
+          bulletActiveClass: 'info-slider__bullet-active'
+        },
+      
+        navigation: {
+          nextEl: '.info-slider__right',
+          prevEl: '.info-slider__left',
+        },
+    });
+
+    const repairTractorsSlider = new Swiper('.repair-tractors__slider', {
+        loop: false,
+        slidesPerView: '1.02',
+        spaceBetween:3,
+    });
+
+    const beforeAfterSlider = new Swiper('.before-after__slider', {
+        loop: true,
+        autoplay:{
+            delay:1000,
+        },
+        speed:2000,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade:true,
+        },
+    });
+
+    const docsSlider = new Swiper('.documents__slider', {
+        loop: false,
+        slidesPerView: '1.15',
+        spaceBetween:20,
+    });
+    //gallery
+
+    //active menu
+
+    //exhibition video interactive with buttons
+    /* const videoBlock = document.querySelector('.exhibition__video'),
+        video = videoBlock.querySelector('video'),
+        play = document.querySelector('.exhibition__play');
+
+    play.addEventListener('click',()=>{
+        video.play();
+    });
+    video.addEventListener('play',()=>{
+        play.style.display = 'none';
+
+    });
+    video.addEventListener('pause',()=>{
+        play.style.display = 'block';
+    });
+    videoBlock.addEventListener('mouseenter',()=>{
+        video.controls = true;
+    });
+    videoBlock.addEventListener('mouseleave',()=>{
+        video.controls = false;
+    }); */
+
+
+    //carousel
+    const images = document.querySelectorAll('.works__carousel-img');
+
+    images.forEach((item)=>{
+        item.addEventListener('mouseenter',(e)=>{
+            target = e.target;
+            images.forEach(item=>{
+                if(target != item){
+                    item.style.filter = 'blur(10px)';
+                    item.style.zIndex = -1;
+                }
+            });
+        });
+        item.addEventListener('mouseleave',()=>{
+            images.forEach(item=>{
+                item.style.filter = 'blur(0px)';
+                item.style.zIndex = 0;
+            });
+        });
+    });
 });
