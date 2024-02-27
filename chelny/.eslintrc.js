6ea2be3fcac7322eeb0c33676efbaedff5842d02
Vladimir_Nechaev <rameforfrontend@gmail.com>
2024-02-27 21:29:11 +0300

    add chelny

---
+++ chelny/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'off',
+			'tab'
+		],
+		'linebreak-style': [
+			'off',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'no-unused-vars': [
+			'warn'
+		],
+		'no-undef': [
+			'off'
+		]
+	}
+};
---
+++ chelny/README.md
@@ -0,0 +1,1 @@
+http://www.chelny-kirovec.ru/
