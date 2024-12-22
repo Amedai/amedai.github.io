faa52d75c3038b4ad26061cbac4a6ffd55575b75
Vladimir_Nechaev <rameforfrontend@gmail.com>
2024-12-22 14:45:12 +0300

    lamkov

--- babaeva/scripts/utils/defineScrollBarWidthCSSVar.js
+++ babaeva/scripts/utils/defineScrollBarWidthCSSVar.js
@@ -1,4 +1,7 @@
 const defineScrollBarWidthCSSVar = () => {
+  console.log(document.documentElement.clientWidth);
+  console.log(window.innerWidth);
+
   document.documentElement.style.setProperty(
     '--scrollbar-width',
     `${window.innerWidth - document.documentElement.clientWidth}px`
