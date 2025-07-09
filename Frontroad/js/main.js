3937e209a9fbc5f3b6f64c201911da84d990f495
Vladimir_Nechaev <rameforfrontend@gmail.com>
2025-07-09 19:36:53 +0300

    frontroad

---
+++ Frontroad/js/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    const stageBlocks = document.querySelectorAll('.roadmap-stage__block');
+
+    stageBlocks.forEach(block=>{
+        const point = block.querySelector('.roadmap-stage__point');
+        const modal = block.querySelector('.roadmap-stage__modal');
+        const closeEl = block.querySelector('.roadmap-stage__modal-close');
+
+        point.addEventListener('click', ()=>{
+            modal.style.display = 'block';
+        });
+
+        closeEl.addEventListener('click',()=>{
+            modal.style.display = 'none';
+        });
+        document.addEventListener('keydown',(e)=>{
+            if(modal.style.display === 'block' && e.code == 'Escape'){
+                console.log(1);
+                modal.style.display = 'none';
+            }
+         });
+    });
+});
